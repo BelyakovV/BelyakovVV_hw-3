57ecddacf960ec83957b2d3f752d9e34e9ff6ce3
Belyakov Vitaliy <doglvill@bk.ru>
2020-09-18 15:08:10 +0300

    done

--- HelloWord.cpp
+++ HelloWord.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
-    cout<<'Hello_Word'<<endl;
-=======
-<<<<<<< HEAD
     cout<<'My_new_rating'<<endl;
-=======
     cout<<'Comment was added'<<endl;
->>>>>>> comment
->>>>>>> rating
+    cout<<'Hello_Word'<<endl;
 }
